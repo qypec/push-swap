209f0f3c9da47ec5d8bbcbd1169872f1fc0f38d9
qypec <qypec.prog@yandex.ru>
2019-11-08 16:59:40 +0300

    added a function to execute a set of operation,a basic plan for checker program

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -6,28 +6,54 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 18:48:56 by yquaro            #+#    #+#             */
-/*   Updated: 2019/11/06 19:22:42 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/11/08 16:57:32 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int						main(int argc, char **argv)
+static void				error_processing(char ***operations)
 {
-	size_t				i;
-	int					number[argc];
-	char				**operations;
+	ft_matrdel(operations);
+	ft_putendl(ERROR_MSG_BAD_OPERATION);
+	exit(-1);
+}
+
+static void				validate(int **stack_a, int **stack_b)
+{
+	// check validity
+}
+
+static char				**get_operations(void)
+{
 	t_buff				*buffer;
 	char				symb;
+	char				**operations;
 
-	i = 0;
-	while (i < argc - 1)
-	{
-		number[i] = ft_atoi(argv[i + 1]);
-		i++;
-	}
 	buffer = ft_buffinit(100);
 	while (read(0, &symb, 1) > 0)
 		ft_buffaddsymb(buffer, symb);
 	operations = ft_strsplit(buffer->str, '\n');
+	ft_buffdel(&buffer);
+	return (operations);
+}
+
+int						main(int argc, char **argv)
+{
+	size_t				i;
+	int					stack_a[argc];
+	int					stack_b[argc];
+	char				**operations;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		stack_a[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
+	operations = get_operations();
+	if ((execute_operations(&stack_a, &stack_b, operations)) == NULL)
+		error_processing(&operations);
+	validate_stacks(&stack_a, s&tack_b);
+	ft_matrdel(&operations);
 }
---
+++ src/operations/execute.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/08 15:06:49 by yquaro            #+#    #+#             */
+/*   Updated: 2019/11/08 15:30:24 by yquaro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void				*get_r_operation(char *operation)
+{
+	if (ft_strequ(operation, "ra"))
+		return (&rotate_a);
+	else if (ft_strequ(operation, "rb"))
+		return (&rotate_b);
+	else if (ft_strequ(operation, "rr"))
+		return (&rotate_ab);
+	else if (ft_strequ(operation, "rra"))
+		return (&reverse_rotate_a);
+	else if (ft_strequ(operation, "rrb"))
+		return (&reverse_rotate_b);
+	else if (ft_strequ(operation, "rrr"))
+		return (&reverse_rotate_ab);
+	else
+		return (NULL);
+}
+
+static void				*get_p_operation(char *operation)
+{
+	if (ft_strequ(operation, "pa"))
+		return (&push_a);
+	else if (ft_strequ(operation, "pb"))
+		return (&push_b);
+	else
+		return (NULL);
+}
+
+static void				*get_s_operation(char *operation)
+{
+	if (ft_strequ(operation, "sa"))
+		return (&swap_a);
+	else if (ft_strequ(operation, "sb"))
+		return (&swap_b);
+	else if (ft_strequ(operation, "ss"))
+		return (&swap_ab);
+	else
+		return (NULL);
+}
+
+static void				*get_pointer_to_operation(char *operation)
+{
+	if (operation[0] == 's')
+		return (get_s_operation(operation));
+	else if (operation[0] == 'p')
+		return (get_p_operation(operation));
+	else if (operation[0] == 'r')
+		return (get_r_operation(operation));
+	else
+		return (NULL);
+}
+
+void					*execute_operations(int **stack_a, int **stack_b, \
+							char **operations)
+{
+	size_t				i;
+	void				(*exec)(int **, int **);
+
+	i = 0;
+	while (operations[i] != NULL)
+	{
+		if ((exec = get_pointer_to_operation(operations[i])) == NULL)
+			return (NULL);
+		exec(stack_a, stack_b);
+		i++;
+	}
+}
