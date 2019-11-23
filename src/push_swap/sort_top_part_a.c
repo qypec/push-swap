904817b5a43b516f93a45b42eb8f372efe37f3b4
qypec <qypec.prog@yandex.ru>
2019-11-23 04:16:07 +0300

    optimized triplet sorting

--- src/push_swap/sort_top_part_a.c
+++ src/push_swap/sort_top_part_a.c
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 23:36:00 by yquaro            #+#    #+#             */
-/*   Updated: 2019/11/23 02:10:32 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/11/23 04:04:16 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,27 @@ static void				check_to_swap(t_stack *stack)
 {
 	if (stack->a->arr[0]->correct_position == 2 && \
 			stack->a->arr[1]->correct_position == 1)
-	{
 		swap_a(stack);
-		add_operation(stack->operations, "sa");
+}
+
+static void				sort_triplet_with_all_operations(t_stack *stack)
+{
+	if (stack->a->arr[2]->correct_position == 3)
+		check_to_swap(stack);
+	else if (stack->a->arr[0]->correct_position == 3)
+	{
+		rotate_a(stack);
+		check_to_swap(stack);
+	}
+	else
+	{
+		if (stack->a->arr[2]->correct_position == 1)
+			reverse_rotate_a(stack);
+		else
+		{
+			swap_a(stack);
+			rotate_a(stack);
+		}
 	}
 }
 
@@ -29,35 +47,27 @@ static void				sort_triplet_ascending(t_stack *stack)
 	else if (stack->a->arr[0]->correct_position == 3)
 	{
 		swap_a(stack);
-		add_operation(stack->operations, "sa");
 		rotate_a(stack);
-		add_operation(stack->operations, "ra");
 		swap_a(stack);
-		add_operation(stack->operations, "sa");
 		reverse_rotate_a(stack);
-		add_operation(stack->operations, "rra");
 		check_to_swap(stack);
 	}
 	else
 	{
 		rotate_a(stack);
-		add_operation(stack->operations, "ra");
 		swap_a(stack);
-		add_operation(stack->operations, "sa");
 		reverse_rotate_a(stack);
-		add_operation(stack->operations, "rra");
 		check_to_swap(stack);
 	}
 }
 
 void					sort_top_part_a(t_stack *stack, size_t border)
 {
-
-	if (border == 1) // need?
-		return ;
 	fill_correct_position(stack->a, border);
 	if (border == 2)
 		check_to_swap(stack);
+	else if (border == stack->a->used_size)
+		sort_triplet_with_all_operations(stack);
 	else
 		sort_triplet_ascending(stack);
 }
--- src/push_swap/sort_top_part_b.c
+++ src/push_swap/sort_top_part_b.c
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 00:24:34 by yquaro            #+#    #+#             */
-/*   Updated: 2019/11/23 02:13:38 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/11/23 04:11:12 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ static void				check_to_swap_first_second(t_stack *stack)
 {
 	if (stack->b->arr[0]->correct_position == 1 && \
 			stack->b->arr[1]->correct_position == 2)
-	{
 		swap_b(stack);
-		add_operation(stack->operations, "sb");
-	}
 }
 
 static void				check_to_swap(t_stack *stack)
@@ -28,11 +25,27 @@ static void				check_to_swap(t_stack *stack)
 			stack->b->arr[1]->correct_position == 1)
 	{
 		rotate_b(stack);
-		add_operation(stack->operations, "rb");
 		swap_b(stack);
-		add_operation(stack->operations, "sb");
 		reverse_rotate_b(stack);
-		add_operation(stack->operations, "rrb");
+	}
+}
+
+static void				sort_triplet_with_all_operations(t_stack *stack)
+{
+	if (stack->b->arr[0]->correct_position == 3)
+		check_to_swap(stack);
+	else if (stack->b->arr[1]->correct_position == 3)
+	{
+		if (stack->b->arr[0]->correct_position == 1)
+			rotate_b(stack);
+		else
+			swap_b(stack);
+	}
+	else
+	{
+		if (stack->b->arr[0]->correct_position == 1)
+			swap_b(stack);
+		reverse_rotate_b(stack);
 	}
 }
 
@@ -43,31 +56,25 @@ static void				sort_triplet_descending(t_stack *stack)
 	else if (stack->b->arr[1]->correct_position == 3)
 	{
 		swap_b(stack);
-		add_operation(stack->operations, "sb");
 		check_to_swap(stack);
 	}
 	else
 	{
 		check_to_swap_first_second(stack);
 		rotate_b(stack);
-		add_operation(stack->operations, "rb");
 		swap_b(stack);
-		add_operation(stack->operations, "sb");
 		reverse_rotate_b(stack);
-		add_operation(stack->operations, "rrb");
 		swap_b(stack);
-		add_operation(stack->operations, "sb");
 	}
 }
 
 void					sort_top_part_b(t_stack *stack, size_t border)
 {
-
-	if (border == 1) // need?
-		return ;
 	fill_correct_position(stack->b, border);
 	if (border == 2)
 		check_to_swap_first_second(stack);
+	else if (border == stack->b->used_size)
+		sort_triplet_with_all_operations(stack);
 	else
 		sort_triplet_descending(stack);
 }
